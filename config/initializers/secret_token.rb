38d7b73c00588a77a30edd42e8d6494902419cee
neil patel <neil@myindianweddingapp.com>
2014-03-29 20:55:03 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Idealprospects::Application.config.secret_key_base = '162e355b6813de830e6e015ac61cb1f3c739bebdf516d60adc14b69f32c57ba66fe908c384061cc83b924066dcf55716a6895ac030b896278afc73e54b8d86c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Idealprospects::Application.config.session_store :cookie_store, key: '_idealprospects_session'
