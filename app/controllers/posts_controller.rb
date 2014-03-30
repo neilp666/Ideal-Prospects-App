2bf5aed212bf7d8280b461b2d1ed2ee5bff84eec
neil patel <neil@myindianweddingapp.com>
2014-03-30 20:58:31 +0100

    removed Json references & changed the show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,39 +26,37 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
+    
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+        redirect_to @post, notice: 'Post was successfully created.' 
+    
       else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render action: 'new' 
+ 
       end
-    end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
+
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @post, notice: 'Post was successfully updated.'
+     
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+       render action: 'edit' 
+   
       end
-    end
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+   
+    redirect_to posts_url
+   
+  
   end
 
   private
