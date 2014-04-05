54b63e5c17b6cd10f48482b389691decc0713eef
neil patel <neil@myindianweddingapp.com>
2014-04-05 11:40:14 +0100

    added paperclip code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ActiveRecord::Base
 
 
 	validates :company, :contact, :email, :telephone, :website, presence: true
-
 	belongs_to :posts
 
+	has_attached_file :avatar,
 	:storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 end
